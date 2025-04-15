17fa683ae748075268a5620bae20d30c0441c6c6
Not-Sarthak <notsarthakshah@gmail.com>
2025-04-15 23:40:45 +0530

    fix: marketplace

--- marketplace/programs/marketplace/src/instructions/purchase.rs
+++ marketplace/programs/marketplace/src/instructions/purchase.rs
@@ -1,26 +1,28 @@
-use anchor_lang::prelude::*;
+use crate::state::{marketplace::Marketplace, listing::Listing};
+use anchor_lang::{prelude::*, system_program::{Transfer, transfer}};
 use anchor_spl::{
     associated_token::AssociatedToken,
     metadata::{MasterEditionAccount, Metadata, MetadataAccount},
-    token::{transfer, Transfer},
+    token::{transfer_checked, TransferChecked},
     token_interface::{Mint, TokenAccount, TokenInterface},
 };
-use crate::state::{Listing, Marketplace};
 
 #[derive(Accounts)]
 pub struct Purchase<'info> {
     #[account(mut)]
     pub taker: Signer<'info>,
-
     #[account(mut)]
-    pub maker: AccountInfo<'info>,
+    pub maker: SystemAccount<'info>,
 
     #[account(
         seeds = [b"marketplace", marketplace.name.as_str().as_bytes()],
         bump = marketplace.bump,
     )]
     pub marketplace: Account<'info, Marketplace>,
-    
+
+    #[account(mut)]
+    pub treasury: SystemAccount<'info>,
+
     pub maker_mint: InterfaceAccount<'info, Mint>,
 
     #[account(
@@ -28,6 +30,7 @@ pub struct Purchase<'info> {
         payer = taker,
         associated_token::mint = maker_mint,
         associated_token::authority = taker,
+        associated_token::token_program = token_program,
     )]
     pub taker_ata: InterfaceAccount<'info, TokenAccount>,
 
@@ -36,81 +39,66 @@ pub struct Purchase<'info> {
         payer = taker,
         associated_token::mint = reward_mint,
         associated_token::authority = taker,
+        associated_token::token_program = token_program,
     )]
     pub taker_rewards_ata: InterfaceAccount<'info, TokenAccount>,
 
     #[account(
-        init,
-        payer = maker,
+        init_if_needed, 
+        payer = taker,
         associated_token::mint = maker_mint,
         associated_token::authority = listing,
+        associated_token::token_program = token_program,
     )]
     pub vault: InterfaceAccount<'info, TokenAccount>,
 
     #[account(
-        mut, 
-        close = maker,
+        mut,
         seeds = [marketplace.key().as_ref(), maker_mint.key().as_ref()],
-        bump,
+        bump = listing.bump,
     )]
     pub listing: Account<'info, Listing>,
 
-    // Missed Here
-
     pub collection_mint: InterfaceAccount<'info, Mint>,
-    #[account(
-        seeds = [
-            b"metadata",
-            metadata_program.key().as_ref(),
-            maker_mint.key().as_ref(),
-        ],
-        seeds::program = metadata_program.key(),
-        bump,
-        constraint = metadata.collection.as_ref().unwrap().key.as_ref() == collection_mint.key().as_ref(),
-        constraint = metadata.collection.as_ref().unwrap().verified == true,
-    )]
-    pub metadata: Account<'info, MetadataAccount>,
 
-    #[account(
-        seeds = [
-            b"metadata",
-            metadata_program.key().as_ref(),
-            maker_mint.key().as_ref(),
-            b"edition"
-        ],
-        seeds::program = metadata_program.key(),
-        bump,
-    )]
-    pub master_edition: Account<'info, MasterEditionAccount>,
+    pub reward_mint: InterfaceAccount<'info, Mint>,
 
-    pub metadata_program: Program<'info, Metadata>,
     pub associated_token_program: Program<'info, AssociatedToken>,
     pub system_program: Program<'info, System>,
     pub token_program: Interface<'info, TokenInterface>,
 }
 
 impl<'info> Purchase<'info> {
-    pub fn send_sol(&mut self) -> Result<()>{
-        let marketplace_fee = (self.marketplace.fee as u64).checked_mul(self.listing.price).unwrap().checked_div(10000_u64).unwrap();
+    pub fn send_sol(&mut self) -> Result<()> {
+        // TODO! Change unwrap with proper error messages, like overflow, underflow etc...
+        let marketplace_fee: u64 = (self.marketplace.fee as u64)
+            .checked_mul(self.listing.price)
+            .unwrap()
+            .checked_div(10000_u64)
+            .unwrap();
 
         let cpi_program = self.system_program.to_account_info();
 
-        let cpi_accounts = Transfer {
+        let cpi_accounts= Transfer {
             from: self.taker.to_account_info(),
             to: self.maker.to_account_info(),
         };
 
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-        
-        let amount = self.listing.price.checked_sub(marketplace_fee).unwrap();
+
+        let amount: u64 = self.listing.price.checked_sub(marketplace_fee).unwrap();
 
         transfer(cpi_ctx, amount)?;
 
-        let cpi_program = self.system_program.to_account_info();
+        let cpi_program: AccountInfo<'_> = self.system_program.to_account_info();
 
-        let cpi_accounts = Transfer {
+        let cpi_accounts: Transfer<'_> = Transfer {
             from: self.taker.to_account_info(),
             to: self.treasury.to_account_info(),
         };
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        transfer(cpi_ctx, marketplace_fee)
     }
 }
